8ed694165c54bdea420c906ee8e9221888b905f7
Josephine Hall <josephinehall@gmail.com>
2014-11-19 23:22:45 +1100

    Add Heracles permissions to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def heracles_admin?
+    true
+  end
+
+  def heracles_superadmin?
+    true
+  end
+
+  def heracles_admin_name
+    email
+  end
 end
