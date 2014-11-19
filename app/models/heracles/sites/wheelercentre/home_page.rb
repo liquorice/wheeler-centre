1ef8d0151c5dc04eb78ccaa992834b61d30fe5b8
Josephine Hall <josephinehall@gmail.com>
2014-11-19 23:12:09 +1100

    Set up basic seeds file and home page model

---
+++ app/models/heracles/sites/wheelercentre/home_page.rb
@@ -0,0 +1,10 @@
+module Heracles
+  module Sites
+    module WheelerCentre
+      class HomePage < ::Heracles::Page
+
+
+      end
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,85 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Make an admin user for Heracles
+user = User.find_or_initialize_by(email: "hello@icelab.com.au")
+user.password = "password1"
+user.save!
+
+# Build the Heracles site
+site = Heracles::Site.find_or_initialize_by(slug: HERACLES_SITE_SLUG)
+site.title = "Wheeler Centre"
+site.hostnames = ["localhost"]
+site.published = true
+site.transloadit_params = {
+  "steps" => {
+    "content_thumbnail_resized" => {
+      "robot"   => "/image/resize",
+      "use"     => ":original",
+      "width"   => 300,
+      "height"  => 300,
+      "quality" => 75,
+      "resize_strategy" => "fillcrop",
+      "gravity" => "center",
+      "zoom"    => true,
+      "strip"   => true,
+    },
+    "content_thumbnail" => {
+      "robot"   => "/image/optimize",
+      "use"     => ["content_thumbnail_resized"],
+    },
+    "content_small_resized" => {
+      "robot"   => "/image/resize",
+      "use"     => ":original",
+      "width"   => 480,
+      "height"  => 720,
+      "quality" => 75,
+      "zoom"    => false,
+      "strip"   => true,
+    },
+    "content_small" => {
+      "robot"   => "/image/optimize",
+      "use"     => ["content_small_resized"],
+    },
+    "content_medium_resized" => {
+      "robot"   => "/image/resize",
+      "use"     => ":original",
+      "width"   => 960,
+      "height"  => 960,
+      "quality" => 75,
+      "zoom"    => false,
+      "strip"   => true,
+    },
+    "content_medium" => {
+      "robot"   => "/image/optimize",
+      "use"     => ["content_medium_resized"],
+    },
+    "content_large_resized" => {
+      "robot"   => "/image/resize",
+      "use"     => ":original",
+      "width"   => 1400,
+      "height"  => 1400,
+      "quality" => 75,
+      "zoom"    => false,
+      "strip"   => true,
+    },
+    "content_large" => {
+      "robot"   => "/image/optimize",
+      "use"     => ["content_large_resized"],
+    },
+    "store" => {
+      "use" => ["content_thumbnail", "content_small", "content_medium", "content_large"],
+    }
+  }
+}
+site.save!
+
+# Homepage
+homepage = Heracles::Sites::WheelerCentre::HomePage.find_or_initialize_by(url: "home")
+homepage.site = site
+homepage.title = "Home"
+homepage.slug = "home"
+homepage.published = true
+homepage.locked = true
+homepage.page_order_position = :first
+homepage.save!
