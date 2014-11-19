fef7d0a4b1f3820e54f17660e84ef910a894acd5
Josephine Hall <josephinehall@gmail.com>
2014-11-19 23:23:08 +1100

    Set up admin routes and after sign in redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   WheelerCentre::Application.routes.draw do
+	  # Heracles admin
 	  mount Heracles::Admin::Engine, at: "/admin"
+
+	  # Admin
+	  resource :admin, only: [:show]
 	end
 end
