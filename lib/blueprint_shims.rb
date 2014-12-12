f83b5a9337178c59a5d5e24824d082a3fca31a69
Josephine Hall <josephinehall@gmail.com>
2014-12-12 17:46:36 +1100

    Add all legacy Blueprint classes to shim

--- lib/blueprint_shims.rb
+++ lib/blueprint_shims.rb
@@ -14,9 +14,63 @@ module LegacyBlueprint
   end
 
   %i(
-    CenevtEvent
     Page
-    Site
+    CenevtPage
+    FaqPage
+    PslPage
+    CttPage
+    SchPage
+    DirPage
+    CendirPage
+    TumPage
+    DbyPage
+    CenvidPage
+    CenmemPage
+    CmtPage
+    LnkPage
+    CencalPage
+    CenplaPage
+    Asset
+    CencalEvent
+    CenevtPresentation
+    CenevtPresenter
+    CenevtProgram
+    CenevtSponsor
+    CenevtSponsorship
+    CenevtVenue
+    CenmemMemberInterest
+    CenmemMemberInterestCategory
+    CenplaBook
+    CenvidJob
+    CenvidNotification
+    CenvidPost
+    CenvidVideo
+    CttRecipient
+    DefaultSetting
+    DirListing
+    Draft
+    EvtEvent
+    CenevtEvent
+    FaqQuestion
+    Geocode
+    LnkLink
+    MemActivity
+    MemCategory
+    CenmemMember
+    DbyPost
+    Pathprint
+    PslPerson
+    Role
+    Setting
+    Tag
+    TagPhrase
+    Thumbnail
+    TumWidget
+    TumArticle
+    TumQuote
+    TumLink
+    TumImage
+    TumSound
     User
   ).each do |legacy_class|
     eval "class #{legacy_class} < Base; end"
