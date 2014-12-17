2528ad46eff9e5336f2e0a1365feb75cbadad1ef
Max Wheeler <max@makenosound.com>
2014-12-17 18:18:08 +1100

    Adjust the video-player format to limit its height and add to the view loader functions.

--- app/insertables/heracles/sites/wheeler_centre/video_insertable_renderer.rb
+++ app/insertables/heracles/sites/wheeler_centre/video_insertable_renderer.rb
@@ -16,12 +16,16 @@ module Heracles
           # Render nothing if the video or it's embed data aren't present
           return "" unless @video && @insertable_data["embedData"].present?
 
-          # ratio = @insertable_data["embedData"]["height"].to_f / @insertable_data["embedData"]["width"].to_f
-          # # The ratio is coupled with CSS to make the embed responsive
-          # video = content_tag(:div, @insertable_data["embedData"]["html"].html_safe, { class: "responsive-embed", style: "padding-bottom: #{ratio * 100}%" })
-          video = content_tag(:div, @insertable_data["embedData"]["html"].html_safe)
+          ratio = @insertable_data["embedData"]["height"].to_f / @insertable_data["embedData"]["width"].to_f
+          # The ratio is coupled with CSS to make the embed responsive
+          video = content_tag(:div, @insertable_data["embedData"]["html"].html_safe, { class: "responsive-embed", style: "padding-bottom: #{ratio * 100}%" })
+          video_content = content_tag :div, class: "video-player__content" do
+            video_content_inner = content_tag :div, video, class: "video-player__content-inner"
+            [video_content_inner].join.html_safe
+          end
 
-          video_cover = content_tag :div, {class: "video-player__cover", "on-click" => "onCoverClick"} do
+          video_teaser = content_tag :div, {class: "video-player__teaser", "on-click" => "onTeaserClick"} do
+            cover = content_tag :div, nil, {class: "video-player__cover"}
             metadata = content_tag :div, class: "video-player__metadata" do
               watch    = content_tag :span, "Watch", {class: "video-player__watch"}
               duration = content_tag :span, "", {class: "video-player__duration", "format-duration" => "duration"}
@@ -34,11 +38,11 @@ module Heracles
               [icon, title].join.html_safe
             end
 
-            [header, metadata].join.html_safe
+            [cover, header, metadata].join.html_safe
           end
 
 
-          video_wrapper = content_tag(:div, [video_cover, video].join.html_safe, {class: "video-player {(playing) ? 'video-player--playing' : ''} {(loaded) ? 'video-player--loaded' : ''}", data: {"view-video-player" => true }})
+          video_wrapper = content_tag(:div, [video_teaser, video_content].join.html_safe, {class: "video-player {(playing) ? 'video-player--playing' : ''} {(loaded) ? 'video-player--loaded' : ''}", data: {"view-video-player" => true }})
           caption = ""
           if @insertable_data[:caption].present?
             caption = content_tag(:div, content_tag(:p, @insertable_data[:caption]), class: "figure__caption copy")
