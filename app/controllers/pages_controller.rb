d81eb6d1c6f684d80b7450b8552b47c3d4865c76
Max Wheeler <max@makenosound.com>
2014-12-24 21:50:08 +1100

    Basic page titles/metadata through the PagesController.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,20 @@
 class PagesController < ApplicationController
   include Heracles::PublicPagesController
+  include ApplicationHelper
+
+  before_filter :set_page_meta_tags, only: [:show]
 
   def site
     @site ||= Heracles::Site.where(slug: HERACLES_SITE_SLUG).first!
+  end
+
+  protected
+
+  def set_page_meta_tags
+    set_meta_tags title: markdown_line(@page.title),
+                  og: {
+                    title: markdown_line(@page.title),
+                    url:   "http://#{@site.hostnames.first}#{@page.absolute_url}"
+                  }
   end
 end
