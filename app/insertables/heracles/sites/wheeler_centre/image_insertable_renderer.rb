52a7c3c36e7adbc2d47f5eb587c5571abe957959
Max Wheeler <max@makenosound.com>
2015-01-08 15:10:04 +1100

    Add attribution and the `content_large` size to the image insertable renderer.

--- app/insertables/heracles/sites/wheeler_centre/image_insertable_renderer.rb
+++ app/insertables/heracles/sites/wheeler_centre/image_insertable_renderer.rb
@@ -19,6 +19,7 @@ module Heracles
 
           small_version_name  = :content_small
           medium_version_name = :content_medium
+          large_version_name =  :content_large
           small_version_name  = :original unless @asset.versions.include?(:content_small)
           medium_version_name = :original unless @asset.versions.include?(:content_medium)
 
@@ -30,16 +31,29 @@ module Heracles
 
           ie9_leader = "<!--[if IE 9]><video style='display: none;'><![endif]-->".html_safe
           ie9_trailer = "<!--[if IE 9]></video><![endif]-->".html_safe
-          medium_source = content_tag :source, nil, {srcset: @asset.send(:"#{medium_version_name}_url"), media: "(min-width: 480px)"}
+          large_source  = content_tag :source, nil, {srcset: @asset.send(:"#{large_version_name}_url"), media: "(min-width: 1200px)"}
+          medium_source = content_tag :source, nil, {srcset: @asset.send(:"#{medium_version_name}_url"), media: "(min-width: 640px)"}
           default_image = content_tag :img, nil, {src: @asset.send(:"#{small_version_name}_url"), alt: @insertable_data[:alt_text], class: "figure__content"}
-          picture = content_tag :picture, [ie9_leader, medium_source, ie9_trailer, default_image].join.html_safe
 
-          caption = ""
-          if @insertable_data[:caption].present?
-            caption = content_tag(:div, content_tag(:p, @insertable_data[:caption]), class: "figure__caption copy")
+          # Assemble!
+          output = []
+          output << content_tag(:picture, [ie9_leader, large_source, medium_source, ie9_trailer, default_image].join.html_safe)
+
+          if @insertable_data[:caption].present? || (@insertable_data[:show_attribution] && @asset.attribution.present?)
+            caption_text = ""
+            if @insertable_data[:caption].present?
+              caption_text += @insertable_data[:caption]
+              if @insertable_data[:show_attribution] && @asset.attribution.present?
+                caption_text += " — "
+              end
+            end
+            if @insertable_data[:show_attribution] && @asset.attribution.present?
+              caption_text += @asset.attribution
+            end
+            output << content_tag(:div, content_tag(:p, caption_text), class: "figure__caption copy")
           end
 
-          content_tag :div, [picture, caption].join.html_safe, {class: "figure figure__display--#{(@insertable_data[:display] || "default").downcase} #{aspect_class}", style: ("width:#{@insertable_data[:width]}" if @insertable_data[:width])}
+          content_tag :div, output.join.html_safe, {class: "figure figure__display--#{(@insertable_data[:display].present? ? @insertable_data[:display] : "default").downcase} #{aspect_class}", style: ("width:#{@insertable_data[:width]}" if @insertable_data[:width])}
         end
       end
     end
