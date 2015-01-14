94a8f6ba763302226e5e1246f4f2715859c23068
Tim Riley <tim@openmonkey.com>
2015-01-14 16:59:37 +1100

    Fix Topic#pages to return the actual pages, not just the topics again

--- app/models/heracles/sites/wheeler_centre/topic.rb
+++ app/models/heracles/sites/wheeler_centre/topic.rb
@@ -15,7 +15,13 @@ module Heracles
 
         def pages
           insertion_keys_for_self_and_descendents = [insertion_key] + children.of_type(page_type).select(:id, :type).map(&:insertion_key)
-          Heracles::Page.joins(:inserteds).where(:"insertions.field" => "topics", :insertion_key => insertion_keys_for_self_and_descendents)
+
+          Heracles::Page.
+            joins(:insertions).
+            where(
+              :"insertions.field" => "topics",
+              :"insertions.inserted_key" => insertion_keys_for_self_and_descendents).
+            group("pages.id") # we can't use `distinct` here since it fails for queries returning JSON column
         end
       end
     end
