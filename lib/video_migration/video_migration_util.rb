68fbb1ec7d66135c7f5bd3203e5d37a17ae2ad09
Josephine Hall <josephinehall@gmail.com>
2015-01-15 18:48:41 +1100

    Update Youtube auth method
    I changed this code in an effort to get the authentication to Youtube working correctly. Sadly, it wasn’t successful and it seems I have to use ClienID based OAuth instead of a service account.

--- lib/video_migration/video_migration_util.rb
+++ lib/video_migration/video_migration_util.rb
@@ -10,7 +10,7 @@ require "trollop"
 require "yaml"
 
 class VideoMigrationUtil
-  YOUTUBE_READ_WRITE_SCOPE = 'https://www.googleapis.com/auth/youtube.upload'
+  YOUTUBE_READ_WRITE_SCOPE = 'https://www.googleapis.com/auth/youtube'
   YOUTUBE_API_SERVICE_NAME = 'youtube'
   YOUTUBE_API_VERSION = 'v3'
 
@@ -27,27 +27,30 @@ class VideoMigrationUtil
     key_file = @opts['key_file']                            # File containing your private key
     key_secret = @opts['key_secret']                        # Password to unlock private key
 
-    @client = Google::APIClient.new(
-      :application_name => @opts['application_name'],
-      :application_version => @opts['application_version'])
-
     # Load our credentials for the service account
     key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
 
-    @client.authorization = Signet::OAuth2::Client.new(
+    @auth_client = Signet::OAuth2::Client.new(
       :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
       :audience => 'https://accounts.google.com/o/oauth2/token',
       :scope => YOUTUBE_READ_WRITE_SCOPE,
       :issuer => service_account_email,
       :signing_key => key)
 
-    # Request a token for our service account
-    @client.authorization.fetch_access_token!
+    @auth_client.fetch_access_token!
+
+    @api_client = Google::APIClient.new(
+      :application_name => @opts['application_name'],
+      :application_version => @opts['application_version'])
+    @api_client.authorization = nil
+
+    @youtube = @api_client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
   end
 
+
   def upload_video(public_url, video)
-    youtube = @client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+    @auth_client.refresh_token
 
     options = Trollop::options do
       opt :file, 'Video File',
@@ -81,13 +84,14 @@ class VideoMigrationUtil
     }
 
     # Call the API's videos.insert method to create and upload the video.
-    videos_insert_response = @client.execute!(
-      :api_method => youtube.videos.insert,
+    videos_insert_response = @api_client.execute!(
+      :api_method => @youtube.videos.insert,
       :body_object => body,
       :media => Google::APIClient::UploadIO.new(options[:file], 'video/*'),
       :parameters => {
         'uploadType' => 'multipart',
-        :part => body.keys.join(',')
+        :part => body.keys.join(','),
+        :authorization => @auth_client
       }
     )
 
