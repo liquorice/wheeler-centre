0e56db8de2267067b5a2e392ff5a722b047a977a
Max Wheeler <max@makenosound.com>
2015-01-15 15:18:09 +1100

    Topics: Get assoc. pages directly from the DB (what a novel concept).

--- app/models/heracles/sites/wheeler_centre/topic.rb
+++ app/models/heracles/sites/wheeler_centre/topic.rb
@@ -13,15 +13,19 @@ module Heracles
 
         ### Accessors
 
-        def pages
+        def pages(options={})
           insertion_keys_for_self_and_descendents = [insertion_key] + children.of_type(page_type).select(:id, :type).map(&:insertion_key)
 
+          Heracles::Page.connection.execute("SELECT setseed(0.#{Time.current.beginning_of_day.to_i});")
           Heracles::Page.
             joins(:insertions).
             where(
               :"insertions.field" => "topics",
               :"insertions.inserted_key" => insertion_keys_for_self_and_descendents).
-            group("pages.id") # we can't use `distinct` here since it fails for queries returning JSON column
+            group("pages.id"). # we can't use `distinct` here since it fails for queries returning JSON column
+            order("random()").
+            page(options[:page_number] || 1).
+            per(options[:per_page] || 20)
         end
       end
     end
