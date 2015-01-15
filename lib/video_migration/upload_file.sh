d208a8fd8be8057dee7a5a78211d74c45f9ff0a4
Josephine Hall <josephinehall@gmail.com>
2015-01-15 18:46:41 +1100

    Update bash script
    Add commands required to run the script

--- lib/video_migration/upload_file.sh
+++ lib/video_migration/upload_file.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+sudo yum update
+# yes
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python get-pip.py
+
 sudo pip install awscli
-aws s3 sync s3://video.wheelercentre.com/52499_31164_39bb473a52976cddf8778091b17cfda75f4c334b_31164.mp4 ~/video
+
+
+pip install --upgrade google-api-python-client
 
 wget https://github.com/tokland/youtube-upload/archive/master.zip
 unzip master.zip
 cd youtube-upload-master
 sudo python setup.py install
+cd ~
+
+aws s3 sync s3://video.wheelercentre.com/52499_31164_39bb473a52976cddf8778091b17cfda75f4c334b_31164.mp4 ~/video
 
 FILE=~/video/52499_31164_39bb473a52976cddf8778091b17cfda75f4c334b_31164.mp4
 TITLE="title"
@@ -16,5 +26,4 @@ KEYWORDS="keywords"
 YOUTUBEEMAIL=josephinehall@gmail.com
 YOUTUBEPASS=BlcifTotboaT
 
-echo "Processing file"
-youtube-upload --email=$YOUTUBEEMAIL --password=$YOUTUBEPASS  FILE --unlisted --title=$TITLE --category=$CATEGORY --description=$DESCRIPTION --keywords=$KEYWORDS
+youtube-upload --email=$YOUTUBEEMAIL --password=$YOUTUBEPASS  FILE --unlisted --title=$TITLE --category=$CATEGORY --description=$DESCRIPTION --keywords=$KEYWORDS "Processing file"
