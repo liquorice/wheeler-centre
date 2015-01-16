dd47331da61d344eae6094e19115ce872c693e26
Josephine Hall <josephinehall@gmail.com>
2015-01-16 22:38:44 +1100

    Adjust bash script
    Add planning notes to the script and remove commands.
    Instead of uploading the video files to Youtube via the command line (and installing tools to do so) run the uploading from a Ruby file

--- lib/video_migration/upload_file.sh
+++ lib/video_migration/upload_file.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 sudo yum update
 # yes
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-sudo python get-pip.py
-
 sudo pip install awscli
-
-
-pip install --upgrade google-api-python-client
-
-wget https://github.com/tokland/youtube-upload/archive/master.zip
-unzip master.zip
-cd youtube-upload-master
-sudo python setup.py install
-cd ~
 
 aws s3 sync s3://video.wheelercentre.com/52499_31164_39bb473a52976cddf8778091b17cfda75f4c334b_31164.mp4 ~/video
 
-FILE=~/video/52499_31164_39bb473a52976cddf8778091b17cfda75f4c334b_31164.mp4
-TITLE="title"
-CATEGORY="category"
-DESCRIPTION="description"
-KEYWORDS="keywords"
-
-YOUTUBEEMAIL=josephinehall@gmail.com
-YOUTUBEPASS=BlcifTotboaT
-
-youtube-upload --email=$YOUTUBEEMAIL --password=$YOUTUBEPASS  FILE --unlisted --title=$TITLE --category=$CATEGORY --description=$DESCRIPTION --keywords=$KEYWORDS "Processing file"
+# install gems
+# run the following
+# require video_migration_util
+# video_migration_util = VideoMigrationUtil.new
+# video_migration_util.upload_video(file_name, blueprint_video_post)
