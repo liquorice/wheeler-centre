e46bb24e6ce95bc02093ab194ce09afebd511b10
Max Wheeler <max@makenosound.com>
2015-01-16 12:52:37 +1100

    Change topic accessors and add a very basic topic page output for blog posts.

--- app/models/heracles/sites/wheeler_centre/topic.rb
+++ app/models/heracles/sites/wheeler_centre/topic.rb
@@ -13,7 +13,21 @@ module Heracles
 
         ### Accessors
 
-        def pages(options={})
+        def latest_pages(options={})
+          insertion_keys_for_self_and_descendents = [insertion_key] + children.of_type(page_type).select(:id, :type).map(&:insertion_key)
+
+          Heracles::Page.
+            joins(:insertions).
+            where(
+              :"insertions.field" => "topics",
+              :"insertions.inserted_key" => insertion_keys_for_self_and_descendents).
+            group("pages.id"). # we can't use `distinct` here since it fails for queries returning JSON column
+            order("created_at DESC").
+            page(options[:page_number] || 1).
+            per(options[:per_page] || 8)
+        end
+
+        def random_pages(options={})
           insertion_keys_for_self_and_descendents = [insertion_key] + children.of_type(page_type).select(:id, :type).map(&:insertion_key)
 
           Heracles::Page.connection.execute("SELECT setseed(0.#{Time.current.beginning_of_day.to_i});")
