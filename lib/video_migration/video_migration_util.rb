4466ea23236a5930487b8fed59ca83ece4ae54d8
Josephine Hall <josephinehall@gmail.com>
2015-01-16 22:35:26 +1100

    Upadte video migration utility to use ClientID OAuth

--- lib/video_migration/video_migration_util.rb
+++ lib/video_migration/video_migration_util.rb
@@ -1,60 +1,34 @@
 #!/usr/bin/ruby
 
 # Usage
-# >> video_migration_util = VideoMigrationUtil.new( config_file: 'video_migration/config.yml')
-# >> video_migration_util.upload_video(public_url, video)
+# >> video_migration_util = VideoMigrationUtil.new
+# >> video_migration_util.upload_video(file_name, video)
 
 require "rubygems"
 require "google/api_client"
+require_relative "oauth_util"
 require "trollop"
 require "yaml"
 
 class VideoMigrationUtil
-  YOUTUBE_READ_WRITE_SCOPE = 'https://www.googleapis.com/auth/youtube'
+  YOUTUBE_READ_WRITE_SCOPE = 'https://www.googleapis.com/auth/youtube.upload'
   YOUTUBE_API_SERVICE_NAME = 'youtube'
   YOUTUBE_API_VERSION = 'v3'
 
-  attr_accessor :config_file
+  def initialize
+    @auth_client = Google::APIClient.new(:application_name => $0, :application_version => '1.0')
+    @youtube = @auth_client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
-  def initialize(args)
-    args.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
-
-    @opts = YAML.load_file(@config_file)
-
-    service_account_email = @opts['service_account_email']  # Email of service account
-    key_file = @opts['key_file']                            # File containing your private key
-    key_secret = @opts['key_secret']                        # Password to unlock private key
-
-    # Load our credentials for the service account
-    key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
-
-    @auth_client = Signet::OAuth2::Client.new(
-      :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-      :audience => 'https://accounts.google.com/o/oauth2/token',
-      :scope => YOUTUBE_READ_WRITE_SCOPE,
-      :issuer => service_account_email,
-      :signing_key => key)
-
-    @auth_client.fetch_access_token!
-
-    @api_client = Google::APIClient.new(
-      :application_name => @opts['application_name'],
-      :application_version => @opts['application_version'])
-    @api_client.authorization = nil
-
-    @youtube = @api_client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
-
+    @auth_util = CommandLineOAuthHelper.new(YOUTUBE_READ_WRITE_SCOPE)
+    @auth_client.authorization = @auth_util.authorize()
   end
 
 
-  def upload_video(public_url, video)
-    @auth_client.refresh_token
-
+  def upload_video(file_name, video)
+    # TODO: pull these values from a video_data.json that we have created for each file
     options = Trollop::options do
       opt :file, 'Video File',
-            :default => public_url, :type => String
+            :default => '~/' + file_name, :type => String
       opt :title, 'Video title',
             :default => 'Test Title', :type => String
       opt :description, 'Video description',
@@ -84,7 +58,7 @@ class VideoMigrationUtil
     }
 
     # Call the API's videos.insert method to create and upload the video.
-    videos_insert_response = @api_client.execute!(
+    videos_insert_response = @auth_client.execute!(
       :api_method => @youtube.videos.insert,
       :body_object => body,
       :media => Google::APIClient::UploadIO.new(options[:file], 'video/*'),
