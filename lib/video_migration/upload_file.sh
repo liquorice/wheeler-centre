6d5b124a4bea675342c8e01468c512543f416227
Josephine Hall <josephinehall@gmail.com>
2015-01-19 23:27:51 +1100

    Update upload script

--- lib/video_migration/upload_file.sh
+++ lib/video_migration/upload_file.sh
@@ -3,10 +3,16 @@ sudo yum update
 # yes
 sudo pip install awscli
 
-aws s3 sync s3://video.wheelercentre.com/52499_31164_39bb473a52976cddf8778091b17cfda75f4c334b_31164.mp4 ~/video
 
-# install gems
-# run the following
-# require video_migration_util
-# video_migration_util = VideoMigrationUtil.new
-# video_migration_util.upload_video(file_name, blueprint_video_post)
+# Fetch the asset
+aws s3 cp --region ap-southeast-2 s3://video.wheelercentre.com/52499_31164_39bb473a52976cddf8778091b17cfda75f4c334b_31164.mp4 ~/
+
+# Install gems
+gem install "google-api-client"
+gem install "trollop"
+gem install "thin"
+gem install "launchy"
+
+# Run the video migration
+ruby video_migration_util.rb
+
