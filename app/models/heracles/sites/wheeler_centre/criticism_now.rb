10579a1add16a28386979ceb85f84aeed9517ac3
Max Wheeler <max@makenosound.com>
2015-02-01 17:16:21 +1100

    Replicate Blueprint output for Criticism Now templates.

--- app/models/heracles/sites/wheeler_centre/criticism_now.rb
+++ app/models/heracles/sites/wheeler_centre/criticism_now.rb
@@ -17,6 +17,11 @@ module Heracles
         def events
           children.of_type("criticism_now_event")
         end
+
+        def primary_responses
+          child = events.find_by_slug("criticism-in-the-digital-age")
+          child.responses
+        end
       end
     end
   end
