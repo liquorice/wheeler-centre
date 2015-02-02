bf40e1cc8ba44fb713559baef4aefc1dc9588454
Max Wheeler <max@makenosound.com>
2015-02-02 14:50:59 +1100

    Add scope for upcoming events.

--- app/models/heracles/sites/wheeler_centre/events.rb
+++ app/models/heracles/sites/wheeler_centre/events.rb
@@ -13,11 +13,27 @@ module Heracles
           }
         end
 
+        def upcoming_events(options={})
+          search_upcoming_events(options)
+        end
+
         def events(options={})
           search_events(options)
         end
 
         private
+
+        def search_upcoming_events(options={})
+          Sunspot.search(Event) do
+            with :site_id, site.id
+            with :parent_id, id
+            with :published, true
+            with(:start_date_time).greater_than_or_equal_to(Time.zone.now)
+
+            order_by :start_date_time, :asc
+            paginate(page: options[:page] || 1, per_page: options[:per_page] || 1000)
+          end
+        end
 
         def search_events(options={})
           Sunspot.search(Event) do
