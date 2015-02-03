bdeccba29e1a0aba336f3324770b36938c4a073a
query-string <atimofeev@reactant.ru>
2015-02-03 07:35:23 +0700

    Turn on Fastly config.action_controller.asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = ENV["ASSETS_VERSION"]
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -56,7 +56,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  #config.action_controller.asset_host = ENV['ASSET_HOST_PRODUCTION']
+  config.action_controller.asset_host = ENV['ASSET_HOST_PRODUCTION']
+  config.static_cache_control = "public, max-age=#{1.year.to_i}"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
