4787ba66a8ab9710fd4904f299c6c2722c4097af
Josephine Hall <josephinehall@gmail.com>
2015-02-04 14:58:01 +1100

    Check whether encode files exists in a different S3 bucket

--- lib/video_migration/upload_encodes.rb
+++ lib/video_migration/upload_encodes.rb
@@ -4,6 +4,13 @@
 
 require "./s3_util"
 
+def file_exists_at_url(url)
+  uri = URI.parse(url)
+  request = Net::HTTP.new uri.host
+  response = request.request_head uri.path
+  return response.code.to_i == 200
+end
+
 s3 = S3Util.new(bucket_name: "wheeler-centre-heracles", config_file: "config.yml")
 data = File.read("audio_video_encodes.yml")
 audio_video_encodes = YAML.load(data)
@@ -19,23 +26,33 @@ audio_video_encodes.each do |encode|
   audio_uri = URI.parse(audio_file_url)
   audio_file_name = File.basename(audio_uri.path)
 
+  if !file_exists_at_url(audio_file_url)
+    audio_file_url = "https://s3.amazonaws.com/video.wheelercentre.com/#{audio_file_name}"
+  end
+
   video_uri = URI.parse(video_file_url)
   video_file_name = File.basename(video_uri.path)
 
+  if !file_exists_at_url(video_file_url)
+    video_file_url = "https://s3.amazonaws.com/video.wheelercentre.com/#{video_file_name}"
+  end
+
   if audio_file_url
-    unless File.file?(audio_file_name)
-      exec("wget #{audio_file_url}")
-    end
     unless s3.find(audio_file_name)
+      unless File.file?(audio_file_name)
+        system("wget #{audio_file_url}")
+      end
+      sleep 1
       s3.upload(audio_file_name)
     end
   end
 
   if video_file_url
-    unless File.file?(video_file_name)
-      exec("wget #{video_file_url}")
-    end
     unless s3.find(video_file_name)
+      unless File.file?(video_file_name)
+        system("wget #{video_file_url}")
+      end
+      sleep 1
       s3.upload(video_file_name)
     end
   end
