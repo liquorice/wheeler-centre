3c8f73197cdd7359fb8d965edf7184426eb82fc8
Max Wheeler <max@makenosound.com>
2015-02-05 14:15:09 +1100

    Add event series base page and accessors, then spin out the masthead contextual nav into a partial.

--- app/models/heracles/sites/wheeler_centre/event_series.rb
+++ app/models/heracles/sites/wheeler_centre/event_series.rb
@@ -50,11 +50,15 @@ module Heracles
         end
 
         def upcoming_events(options={})
-          search_events(options)
+          search_upcoming_events(options)
+        end
+
+        def past_events(options={})
+          search_past_events(options)
         end
 
         def sponsors
-           @sponsors ||= fields["sponsors"].pages.published
+           @sponsors ||= fields["sponsors"].pages.visible.published
         end
 
         searchable do
@@ -77,18 +81,29 @@ module Heracles
 
         private
 
-        def search_events(options={})
+        def search_upcoming_events(options={})
           Sunspot.search(Event) do
             with :site_id, site.id
             with :event_series_ids, id
             with :published, true
+            with(:start_date_time).greater_than_or_equal_to(Time.zone.now.beginning_of_day)
 
             order_by :start_date, :asc
             paginate page: options[:page] || 1, per_page: options[:per_page] || 18
           end
         end
 
+        def search_past_events(options={})
+          Sunspot.search(Event) do
+            with :site_id, site.id
+            with :event_series_ids, id
+            with :published, true
+            with(:start_date_time).less_than(Time.zone.now.beginning_of_day)
 
+            order_by :start_date, :desc
+            paginate page: options[:page] || 1, per_page: options[:per_page] || 18
+          end
+        end
 
       end
     end
