59fc6c8bcb47eb3a099a331159a7171bc08f1ef1
Max Wheeler <max@makenosound.com>
2015-02-08 23:46:32 +1100

    Add caching headers to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,12 @@ Rails.application.configure do
   #config.action_controller.asset_host = ENV['ASSET_HOST_PRODUCTION']
   config.static_cache_control = "public, max-age=#{1.year.to_i}"
 
+  # Set frontend caching to a day
+  config.action_dispatch.default_headers = {
+    "Surrogate-Control" => "max-age=86400",
+    "Cache-Control" => "max-age=86400, public",
+  }
+
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
