44d57a670962cd6e045a12b0b6d140527bfef263
Max Wheeler <max@makenosound.com>
2015-02-09 00:30:52 +1100

    Try setting Surrogate-Keys for caching?

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,17 @@ class PagesController < ApplicationController
   include ApplicationHelper
 
   before_filter :set_page_meta_tags, only: [:show]
+  before_filter :set_cache_headers_for_pages, only: [:show]
 
   def site
     @site ||= Heracles::Site.where(slug: HERACLES_SITE_SLUG).first!
   end
 
   protected
+
+  def set_cache_headers_for_pages
+    response.headers["Surrogate-Key"] = page.cache_key
+  end
 
   def set_page_meta_tags
     set_meta_tags title: markdown_line(@page.title),
