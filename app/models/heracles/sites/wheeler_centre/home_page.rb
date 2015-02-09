6fda3c60679398963794c73900f8fe9e51ed6c0b
Max Wheeler <max@makenosound.com>
2015-02-10 01:13:00 +1100

    Home page template.

--- app/models/heracles/sites/wheeler_centre/home_page.rb
+++ app/models/heracles/sites/wheeler_centre/home_page.rb
@@ -6,7 +6,29 @@ module Heracles
           {
             fields: [
               {name: :intro, type: :content},
+              # Banners
               {name: :banners, label: "Home page banners", type: :associated_pages, page_type: :home_banner},
+              # Highlights
+              {name: :highlights_info, type: :info, text: "<hr/>"},
+              {name: :highlights_primary_title, type: :text, editor_columns: 6},
+              {name: :highlights_primary_tags, type: :text, editor_type: :code, editor_columns: 6, hint: "Defaults to 'highlights'"},
+              {name: :highlights_primary_content, type: :content, with_buttons: %i(bold italic), disable_insertables: true},
+              {name: :highlights_secondary_title, type: :text, editor_columns: 6},
+              {name: :highlights_secondary_tags, type: :text, editor_type: :code, editor_columns: 6, hint: "Separate multiple tags with a comma"},
+              {name: :highlights_secondary_content, type: :content, with_buttons: %i(bold italic), disable_insertables: true},
+              # Quotes
+              {name: :quotes_info, type: :info, text: "<hr/>"},
+              {name: :quotes_title, type: :text},
+              {name: :quotes_content, type: :content, with_buttons: %i(bold italic), disable_insertables: true},
+              {name: :quotes, label: "Home page quotes", type: :associated_pages, page_type: :home_quote},
+              # Writings
+              {name: :writings_info, type: :info, text: "<hr/>"},
+              {name: :writings_title, type: :text, editor_columns: 6},
+              {name: :writings_tags, type: :text, editor_type: :code, editor_columns: 6, hint: "Separate multiple tags with a comma"},
+              {name: :writings_content, type: :content, with_buttons: %i(bold italic), disable_insertables: true},
+              # Body
+              {name: :body_info, type: :info, text: "<hr/>"},
+              {name: :body, type: :content},
             ]
           }
         end
@@ -16,6 +38,58 @@ module Heracles
         def sorted_banners
           if fields[:banners].data_present?
             fields[:banners].pages.first(9)
+          end
+        end
+
+        # Temporary
+        # Should be replace with saved-search insertables
+        def highlights(options={})
+          options[:tags] = options[:tags].presence || ["highlights"]
+          options.reverse_merge!({tags: options[:tags]})
+          search_by_tag(options)
+        end
+
+        # Temporary
+        # Should be replace with saved-search insertables
+        def user_highlights(options={})
+          options.reverse_merge!({tags: options[:tags]})
+          search_by_tag(options)
+        end
+
+        # Temporary
+        # Should be replace with saved-search insertables
+        def user_writings(options={})
+          options.reverse_merge!({tags: options[:tags]})
+          search_user_writings(options)
+        end
+        private
+
+        def searchable_types
+          [
+            BlogPost,
+            Event,
+            Recording,
+            Person
+          ]
+        end
+
+        def search_by_tag(options={})
+          Sunspot.search(searchable_types.flatten) do
+            with :site_id, site.id
+            with :published, true
+            with :tag_list, options[:tags] if options[:tags].present?
+            order_by :created_at, :desc
+            paginate page: options[:page] || 1, per_page: options[:per_page] || 6
+          end
+        end
+
+        def search_user_writings(options={})
+          Sunspot.search(BlogPost) do
+            with :site_id, site.id
+            with :published, true
+            with :tag_list, options[:tags] if options[:tags].present?
+            order_by :created_at, :desc
+            paginate page: options[:page] || 1, per_page: options[:per_page] || 6
           end
         end
       end
