af455e65d87780b2338b7fedb4a2d910eaa70cc1
Max Wheeler <max@makenosound.com>
2015-02-09 18:07:52 +1100

    Resolve frontent conflicts and fix up some banner CSS.

---
+++ app/models/heracles/sites/wheeler_centre/home_banner.rb
@@ -0,0 +1,31 @@
+module Heracles
+  module Sites
+    module WheelerCentre
+      class HomeBanner < ::Heracles::Page
+        def self.config
+          {
+            fields: [
+              {name: :label, type: :content, hint: "An (optional) short label for the banner", with_buttons: %i(bold italic), disable_insertables: true},
+              {name: :banner_image, type: :asset, asset_file_type: :image, required: true},
+              {name: :link, type: :text, editor_columns: 6, hint: "URL to link to. Ideally should start with a /, like /events/show-of-the-year", editor_type: :code, required: true},
+              {name: :category, type: :text, editor_type: 'select', option_values: [ "Watch", "Listen", "Read", "Attend" ], editor_columns: 3},
+              {name: :size, type: :text, editor_type: 'select', option_values: [ "Small", "Large" ] , editor_columns: 3},
+            ]
+          }
+        end
+
+        ### Summary
+
+        def to_summary_hash
+          {
+            title: title,
+            label: fields[:label],
+            link: fields[:link],
+            published: (published) ? "✔" : "•",
+            created_at:  created_at.to_s(:admin_date)
+          }
+        end
+      end
+    end
+  end
+end
