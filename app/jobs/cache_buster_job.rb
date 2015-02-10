9a3e938a9648b1820bf4e7334af0d7cdb1be82dc
Tim Riley <tim@openmonkey.com>
2015-02-11 10:27:22 +1100

    Tidy up the cache buster job and fix some potential errors
    * It would have purged the cache unnecessarily on first run, when the database entry has no checksum
    * It was comparing the updated_at timestamp to Time.now, which isn’t in the current timezone. Using the relative `5.minutes.ago` fixes this, and is also clearer to read.
    * Adjust the methods in the job class so that they are either (a) commands, or (b) accessors that return a value.
    * Don’t use Nokogiri to load the web page. We don’t need a full HTML parser here. We can just call `#read` on the opened page instead.

--- app/jobs/cache_buster_job.rb
+++ app/jobs/cache_buster_job.rb
@@ -1,42 +1,46 @@
+require "open-uri"
 require 'digest/md5'
 
 class CacheBusterJob < Que::Job
-
   def run(id)
-    @hit = CacheBuster.find(id)
-    @uri = "http://#{ENV['CDN_ORIGIN']}#{@hit.path}"
+    @page_check = CacheBuster.find(id)
+    @uri = "http://#{ENV['CDN_ORIGIN']}#{@page_check.path}"
 
-    request_page
-    purge_page if @hit.checksum != @checksum
-    update_hit
-
-    p "Hit for page #{@hit.path} processed!"
+    purge_page if should_purge_page?
+    update_page_check
   end
 
-  def request_page
+  private
+
+  def should_purge_page?
+    @page_check.checksum.present? && @page_check.checksum != current_page_checksum
+  end
+
+  def purge_page
+    client = Varnisher::Purger.new('PURGE', @page_check.path, ENV['CDN_HOST'])
+    purge = client.send
+  end
+
+  def update_page_check
+    @page_check.update(checksum: current_page_checksum, updated_at: Time.current)
+  end
+
+  memoize \
+  def current_page_checksum
+    Digest::MD5.hexdigest(current_page_contents)
+  end
+
+  memoize \
+  def current_page_contents
     basic_auth_user     = ENV['BASIC_AUTH_USER']
     basic_auth_password = ENV['BASIC_AUTH_PASSWORD']
 
     source = if basic_auth_password && basic_auth_user
-      Nokogiri::HTML(open(
-        @uri,
-        http_basic_authentication: [basic_auth_user, basic_auth_password]
-      ))
+      open(@uri, http_basic_authentication: [basic_auth_user, basic_auth_password])
     else
-      Nokogiri::HTML(open(@uri))
+      open(@uri)
     end
 
-    @checksum = Digest::MD5.hexdigest(source)
+    source.read
   end
-
-  def purge_page
-    client = Varnisher::Purger.new('PURGE', @hit.path, ENV['CDN_HOST'])
-    purge = client.send
-    p "Purging status: #{purge}"
-  end
-
-  def update_hit
-    @hit.update(checksum: @checksum)
-  end
-
 end
