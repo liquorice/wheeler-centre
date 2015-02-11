dbb4b94a1e519f38444560e01bacf57c6fc1fabb
query-string <atimofeev@reactant.ru>
2015-02-11 15:26:31 +0700

    Get rid of `include AbstractController::Rendering` and just set the `response_body`.

--- app/controllers/cache_buster_controller.rb
+++ app/controllers/cache_buster_controller.rb
@@ -1,9 +1,5 @@
 class CacheBusterController < ActionController::Metal
   include ActionController::MimeResponds
-  include AbstractController::Rendering
-  include ActionView::Layouts
-
-  append_view_path "#{Rails.root}/app/views"
 
   def index
     headers['Content-Type'] = 'application/javascript'
@@ -15,7 +11,7 @@ class CacheBusterController < ActionController::Metal
 
       # If it was more than 5 minutes ago (or never before), then it fires off a background job to check the page
       CacheBusterJob.enqueue hit.id if hit.updated_at <= 5.minutes.ago
-      render text: ";(function() { if (typeof console !== 'undefined' && console !== null) { if (typeof console.debug === 'function') { console.debug('Reactive Cache Buster updated at: #{hit.updated_at}'); } } })();"
+      self.response_body = ";(function() { if (typeof console !== 'undefined' && console !== null) { if (typeof console.debug === 'function') { console.debug('Reactive Cache Buster updated at: #{hit.updated_at}'); } } })();"
     end
   end
 end
