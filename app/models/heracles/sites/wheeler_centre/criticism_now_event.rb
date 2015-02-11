a84f0e09f6924f9c4dcc31e41af6aa05cb534f10
Max Wheeler <max@makenosound.com>
2015-02-12 01:47:34 +1100

    Crit now index.

--- app/models/heracles/sites/wheeler_centre/criticism_now_event.rb
+++ app/models/heracles/sites/wheeler_centre/criticism_now_event.rb
@@ -7,7 +7,6 @@ module Heracles
             fields: [
               # Image
               {name: :hero_image, type: :asset, asset_file_type: :image},
-              {name: :intro, label: "Introduction", type: :content},
               {name: :body, type: :content},
               # Listen
               {name: :listen, type: :content},
