406517117f23fb43f312df9a703588d4441fa1df
query-string <atimofeev@reactant.ru>
2015-02-11 18:26:27 +0700

    Use site original url as domain param for cache buster helper.

--- app/helpers/cache_buster_helper.rb
+++ app/helpers/cache_buster_helper.rb
@@ -2,7 +2,7 @@ module CacheBusterHelper
 
   def cache_buster_script
     src = if Rails.env.production?
-      "http://#{@site.origin_hostname}/_check/#{CGI::escape("#{request.original_url}/_check.js")}"
+      cache_buster_check_url("#{request.original_url}/_check.js", domain: @site.original_url)
     else
       cache_buster_check_url("#{request.original_url}/_check.js", debug: true, domain: ENV['CDN_ORIGIN_DOMAIN'], port: ENV['CDN_ORIGIN_PORT'])
     end
