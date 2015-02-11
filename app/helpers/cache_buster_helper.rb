2487dd946d8ced4fc98df1ec61ecda0dec7f6733
query-string <atimofeev@reactant.ru>
2015-02-11 18:09:44 +0700

    Encode URL before sending.

--- app/helpers/cache_buster_helper.rb
+++ app/helpers/cache_buster_helper.rb
@@ -2,7 +2,7 @@ module CacheBusterHelper
 
   def cache_buster_script
     src = if Rails.env.production?
-      "http://#{@site.origin_hostname}/_check/#{request.original_url}/_check.js"
+      "http://#{@site.origin_hostname}/_check/#{CGI::escape("#{request.original_url}/_check.js")}"
     else
       cache_buster_check_url("#{request.original_url}/_check.js", debug: true, domain: ENV['CDN_ORIGIN_DOMAIN'], port: ENV['CDN_ORIGIN_PORT'])
     end
