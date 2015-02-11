05e0c6a72d213ae35ff7f0e98183f91cb085c764
query-string <atimofeev@reactant.ru>
2015-02-11 17:55:01 +0700

    Use helper for rendering cache buster link.

---
+++ app/helpers/cache_buster_helper.rb
@@ -0,0 +1,12 @@
+module CacheBusterHelper
+
+  def cache_buster_script
+    src = if Rails.env.production?
+      "http://#{@site.origin_hostname}/_check/#{request.original_url}/_check.js"
+    else
+      cache_buster_check_url("#{request.original_url}/_check.js", debug: true, domain: ENV['CDN_ORIGIN_DOMAIN'], port: ENV['CDN_ORIGIN_PORT'])
+    end
+    content_tag :script, nil, async: true, src: src
+  end
+
+end
