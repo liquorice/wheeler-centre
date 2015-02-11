1a0b4d19eadc54b7c8a69b3b9445f9b301107254
query-string <atimofeev@reactant.ru>
2015-02-11 16:59:39 +0700

    Rename hit variable.

--- app/controllers/cache_buster_controller.rb
+++ app/controllers/cache_buster_controller.rb
@@ -7,11 +7,11 @@ class CacheBusterController < ActionController::Metal
     unless params[:edge_uri].blank?
       # It checks in the Redis DB to see when the page was last accessed
       edge_uri = params[:edge_uri].gsub('/_check', '')
-      hit = PageCacheCheck.find_or_create_by edge_uri: edge_uri
+      page_check = PageCacheCheck.find_or_create_by edge_uri: edge_uri
 
       # If it was more than 5 minutes ago (or never before), then it fires off a background job to check the page
-      BustPageCacheJob.enqueue hit.id if hit.updated_at <= 5.minutes.ago
-      self.response_body = params[:debug].present? ? ";(function() { if (typeof console !== 'undefined' && console !== null) { if (typeof console.debug === 'function') { console.debug('Reactive Cache Buster updated at: #{hit.updated_at}'); } } })();" : ''
+      BustPageCacheJob.enqueue page_check.id if page_check.updated_at <= 5.minutes.ago
+      self.response_body = params[:debug].present? ? ";(function() { if (typeof console !== 'undefined' && console !== null) { if (typeof console.debug === 'function') { console.debug('Reactive Cache Buster updated at: #{page_check.updated_at}'); } } })();" : ''
     end
   end
 end
