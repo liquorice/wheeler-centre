f4bacfdf0f070430a0a29461e402546054b71b67
Josephine Hall <josephinehall@gmail.com>
2015-02-12 11:59:28 +1100

    Exclude listing pages + pages without templates from site search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -17,6 +17,34 @@ class SearchesController < ApplicationController
         with :page_type, params[:page_type]
       end
 
+      # We never want the listing pages to show up in the search
+      without :page_type, [ "blog",
+                            "broadcasts",
+                            "criticism_now",
+                            "event_series_index",
+                            "events",
+                            "events_archive",
+                            "guests",
+                            "home_page",
+                            "people",
+                            "placeholder",
+                            "podcasts",
+                            "presenters",
+                            "settings",
+                            "sponsors",
+                            "texts_in_the_city",
+                            "topics",
+                            "venues"]
+
+      # Also exclude all the things for which we don't have an individual-level view
+      without :page_type, [ "texts_in_the_city_book",
+                            "home_banner",
+                            "vpla_year",
+                            "zoo_fellowships_work",
+                            "response",
+                            "sponsor",
+                            "itunes_category"]
+
       paginate page: params[:page] || 1, per_page: 40
     end
   end
