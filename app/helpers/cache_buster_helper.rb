20102ee8e8da4c932bfe4c1d09781145f102132c
Tim Riley <tim@openmonkey.com>
2015-02-12 20:33:43 +1100

    Fix logic for when not to include the cache check script

--- app/helpers/cache_buster_helper.rb
+++ app/helpers/cache_buster_helper.rb
@@ -1,7 +1,7 @@
 module CacheBusterHelper
   def cache_buster_script(site)
     return if !site.has_edge_delivery?
-    return if Rails.env.production? && site.edge_hostnames.include?(request.host)
+    return if Rails.env.production? && !site.edge_hostnames.include?(request.host)
 
     host = (request.port == 80 ? request.host : request.host_with_port)
 
