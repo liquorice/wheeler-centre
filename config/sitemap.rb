a61c3b63e250b92e4b9f3db0b840cbefdfbdf7e9
Max Wheeler <max@makenosound.com>
2015-02-17 09:23:24 +1100

    Remove a few things from the sitemap and make it respect `.visible` scopes.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -42,121 +42,119 @@ SitemapGenerator::Sitemap.create do
   end
 
   # Add blog to index
-  blog = Heracles::Sites::WheelerCentre::Blog.published
+  blog = Heracles::Sites::WheelerCentre::Blog.visible.published
   blog.each { |p| add "/#{p.url}" }
 
   # Add blog posts
-  blog_posts = Heracles::Sites::WheelerCentre::BlogPost.published
+  blog_posts = Heracles::Sites::WheelerCentre::BlogPost.visible.published
   blog_posts.each { |p| add "/#{p.url}" }
 
-  book_reviews = Heracles::Sites::WheelerCentre::BookReview.published
+  book_reviews = Heracles::Sites::WheelerCentre::BookReview.visible.published
   book_reviews.each { |p| add "/#{p.url}" }
 
-  broadcasts = Heracles::Sites::WheelerCentre::Broadcasts.published
+  broadcasts = Heracles::Sites::WheelerCentre::Broadcasts.visible.published
   broadcasts.each { |p| add "/#{p.url}" }
 
   # Add all content pages to the sitemap
-  pages = Heracles::Sites::WheelerCentre::ContentPage.published
+  pages = Heracles::Sites::WheelerCentre::ContentPage.visible.published
   pages.each { |p| add "/#{p.url}" }
 
-  criticism_now = Heracles::Sites::WheelerCentre::CriticismNow.published
+  criticism_now = Heracles::Sites::WheelerCentre::CriticismNow.visible.published
   criticism_now.each { |p| add "/#{p.url}" }
 
   # Add events to sitemap
-  events = Heracles::Sites::WheelerCentre::Event.published
+  events = Heracles::Sites::WheelerCentre::Event.visible.published
   events.each { |p| add "/#{p.url}" }
 
   # Add events index
-  events_index = Heracles::Sites::WheelerCentre::Events.published
+  events_index = Heracles::Sites::WheelerCentre::Events.visible.published
   events_index.each { |p| add "/#{p.url}" }
 
   # Add events series to sitemap
-  events_series = Heracles::Sites::WheelerCentre::EventSeries.published
+  events_series = Heracles::Sites::WheelerCentre::EventSeries.visible.published
   events_series.each { |p| add "/#{p.url}" }
 
   # Add events series indicies to sitemap
-  events_series_indicies = Heracles::Sites::WheelerCentre::EventSeriesIndex.published
+  events_series_indicies = Heracles::Sites::WheelerCentre::EventSeriesIndex.visible.published
   events_series_indicies.each { |p| add "/#{p.url}" }
 
-  guests = Heracles::Sites::WheelerCentre::Guests.published
+  guests = Heracles::Sites::WheelerCentre::Guests.visible.published
   guests.each { |p| add "/#{p.url}" }
 
   # Home  - n/a
   # Itunes category - n/a
 
   # Add people index
-  people = Heracles::Sites::WheelerCentre::People.published
+  people = Heracles::Sites::WheelerCentre::People.visible.published
   people.each { |p| add "/#{p.url}"}
 
   # Add persons ;)
-  persons = Heracles::Sites::WheelerCentre::Person.published
+  persons = Heracles::Sites::WheelerCentre::Person.visible.published
   persons.each { |p| add "/#{p.url}" }
 
   # Placeholder n/a
 
-  podcast_episodes = Heracles::Sites::WheelerCentre::PodcastEpisode.published
+  podcast_episodes = Heracles::Sites::WheelerCentre::PodcastEpisode.visible.published
   podcast_episodes.each { |p| add "/#{p.url}"}
 
-  podcast_series = Heracles::Sites::WheelerCentre::PodcastSeries.published
+  podcast_series = Heracles::Sites::WheelerCentre::PodcastSeries.visible.published
   podcast_series.each { |p| add "/#{p.url}"}
 
-  podcasts = Heracles::Sites::WheelerCentre::Podcasts.published
+  podcasts = Heracles::Sites::WheelerCentre::Podcasts.visible.published
   podcasts.each { |p| add "/#{p.url}"}
 
-  presenters = Heracles::Sites::WheelerCentre::Presenters.published
+  presenters = Heracles::Sites::WheelerCentre::Presenters.visible.published
   presenters.each { |p| add "/#{p.url}"}
 
-  recordings = Heracles::Sites::WheelerCentre::Recording.published
+  recordings = Heracles::Sites::WheelerCentre::Recording.visible.published
   recordings.each { |p| add "/#{p.url}"}
 
-  residents = Heracles::Sites::WheelerCentre::Resident.published
-  residents.each { |p| add "/#{p.url}"}
+  # Residents n/a
 
-  responses = Heracles::Sites::WheelerCentre::Response.published
+  responses = Heracles::Sites::WheelerCentre::Response.visible.published
   responses.each { |p| add "/#{p.url}"}
 
-  reviews = Heracles::Sites::WheelerCentre::Review.published
+  reviews = Heracles::Sites::WheelerCentre::Review.visible.published
   reviews.each { |p| add "/#{p.url}"}
 
   # Settings n/a
 
-  sponsors = Heracles::Sites::WheelerCentre::Sponsor.published
-  sponsors.each { |p| add "/#{p.url}"}
+  # Sponsors n/a
 
-  sponsors_index = Heracles::Sites::WheelerCentre::Sponsors.published
+  sponsors_index = Heracles::Sites::WheelerCentre::Sponsors.visible.published
   sponsors_index.each { |p| add "/#{p.url}"}
 
-  texts = Heracles::Sites::WheelerCentre::TextsInTheCity.published
+  texts = Heracles::Sites::WheelerCentre::TextsInTheCity.visible.published
   texts.each { |p| add "/#{p.url}"}
 
-  texts_books = Heracles::Sites::WheelerCentre::TextsInTheCityBook.published
+  texts_books = Heracles::Sites::WheelerCentre::TextsInTheCityBook.visible.published
   texts_books.each { |p| add "/#{p.url}"}
 
-  topics = Heracles::Sites::WheelerCentre::Topic.published
+  topics = Heracles::Sites::WheelerCentre::Topic.visible.published
   topics.each { |p| add "/#{p.url}"}
 
-  topics_index = Heracles::Sites::WheelerCentre::Topics.published
+  topics_index = Heracles::Sites::WheelerCentre::Topics.visible.published
   topics_index.each { |p| add "/#{p.url}"}
 
-  venues = Heracles::Sites::WheelerCentre::Venue.published
+  venues = Heracles::Sites::WheelerCentre::Venue.visible.published
   venues.each { |p| add "/#{p.url}"}
 
-  venues_index = Heracles::Sites::WheelerCentre::Venues.published
+  venues_index = Heracles::Sites::WheelerCentre::Venues.visible.published
   venues_index.each { |p| add "/#{p.url}"}
 
-  vpla_books = Heracles::Sites::WheelerCentre::VplaBook.published
+  vpla_books = Heracles::Sites::WheelerCentre::VplaBook.visible.published
   vpla_books.each { |p| add "/#{p.url}"}
 
-  vpla_categories = Heracles::Sites::WheelerCentre::VplaCategory.published
+  vpla_categories = Heracles::Sites::WheelerCentre::VplaCategory.visible.published
   vpla_categories.each { |p| add "/#{p.url}"}
 
-  vpla_year = Heracles::Sites::WheelerCentre::VplaYear.published
+  vpla_year = Heracles::Sites::WheelerCentre::VplaYear.visible.published
   vpla_year.each { |p| add "/#{p.url}"}
 
-  zoo_fellowships = Heracles::Sites::WheelerCentre::ZooFellowships.published
+  zoo_fellowships = Heracles::Sites::WheelerCentre::ZooFellowships.visible.published
   zoo_fellowships.each { |p| add "/#{p.url}"}
 
-  zoo_fellowships_work = Heracles::Sites::WheelerCentre::ZooFellowshipsWork.published
+  zoo_fellowships_work = Heracles::Sites::WheelerCentre::ZooFellowshipsWork.visible.published
   zoo_fellowships_work.each { |p| add "/#{p.url}"}
 
 end
