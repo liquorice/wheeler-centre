0b8d733072ae9d94a2f4db4538e08c16fa10573b
Max Wheeler <max@makenosound.com>
2015-02-24 18:08:14 +1100

    Allow subscribe popup to be configured from the settings screen. You can:
    * Disable it entirely
    * Add a background image
    * Change the intro text

--- app/models/heracles/sites/wheeler_centre/settings.rb
+++ app/models/heracles/sites/wheeler_centre/settings.rb
@@ -4,7 +4,13 @@ module Heracles
       class Settings < ::Heracles::Page
         def self.config
           {
-            fields: []
+            fields: [
+              # Subscribe popup
+              {name: :subscript_popup_info, type: :info, text: "<hr/>"},
+              {name: :subscribe_popup_intro, type: :content, with_buttons: %i(h2 h3 bold italic link), disable_insertables: true},
+              {name: :subscribe_popup_image, type: :asset, asset_file_type: :image},
+              {name: :subscribe_popup_enabled, label: "Enable subscribe popup", type: :boolean, defaults: {value: true}, question_text: "Show the subscribe popup to new vistors?"},
+            ]
           }
         end
       end
