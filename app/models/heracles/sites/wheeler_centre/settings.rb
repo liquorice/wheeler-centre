bd21714124294246e5ef2f83b61d742c81552e9c
Max Wheeler <max@makenosound.com>
2015-02-25 15:42:21 +1100

    Allow images in the subscribe popup.

--- app/models/heracles/sites/wheeler_centre/settings.rb
+++ app/models/heracles/sites/wheeler_centre/settings.rb
@@ -7,7 +7,7 @@ module Heracles
             fields: [
               # Subscribe popup
               {name: :subscript_popup_info, type: :info, text: "<hr/>"},
-              {name: :subscribe_popup_intro, type: :content, with_buttons: %i(h2 h3 bold italic link), disable_insertables: true},
+              {name: :subscribe_popup_intro, type: :content, with_buttons: %i(h2 h3 bold italic link), with_insertables: %i(image)},
               {name: :subscribe_popup_image, type: :asset, asset_file_type: :image},
               {name: :subscribe_popup_enabled, label: "Enable subscribe popup", type: :boolean, defaults: {value: true}, question_text: "Show the subscribe popup to new vistors?"},
             ]
