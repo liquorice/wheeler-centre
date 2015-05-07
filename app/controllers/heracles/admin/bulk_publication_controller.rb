81d9e7a198dff076934b4bc13a5f88afce863eab
query-string <atimofeev@reactant.ru>
2015-05-07 16:48:47 +0700

    Use facets for displaying types of materials.

--- app/controllers/heracles/admin/bulk_publication_controller.rb
+++ app/controllers/heracles/admin/bulk_publication_controller.rb
@@ -5,15 +5,18 @@ module Heracles
 
       def index
         if params[:q]
-          tags         = params[:q].split(",")
+          tags = params[:q].split(",")
 
           @search = Sunspot.search(Heracles::Site.first.page_classes) do
             with :tags, tags
             with :published, true
             with :site_id, current_site.id
             order_by :created_at, :desc
+            facet :page_type
             paginate page: params[:page] || 1, per_page: 40
           end
+
+          @facets = @search.facet(:page_type).rows
         end
       end
 
