9024b18d3a887b0412d0c2df25dca8541bb3a836
query-string <atimofeev@reactant.ru>
2015-05-07 13:53:02 +0700

    Remove published/unpublished filtering.

--- app/controllers/heracles/admin/bulk_publication_controller.rb
+++ app/controllers/heracles/admin/bulk_publication_controller.rb
@@ -6,11 +6,10 @@ module Heracles
       def index
         if params[:q]
           tags         = params[:q].split(",")
-          search_scope = params[:scope] == "published" ? true : false
 
           @search = Sunspot.search(Heracles::Site.first.page_classes) do
             with :tags, tags
-            with :published, search_scope
+            with :published, true
             with :site_id, current_site.id
             order_by :created_at, :desc
             paginate page: params[:page] || 1, per_page: 40
