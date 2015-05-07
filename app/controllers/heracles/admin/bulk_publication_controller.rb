95fe3c9c4d37afa62d668357c3b088e2a017f0ff
query-string <atimofeev@reactant.ru>
2015-05-07 12:27:10 +0700

    Display search results.

--- app/controllers/heracles/admin/bulk_publication_controller.rb
+++ app/controllers/heracles/admin/bulk_publication_controller.rb
@@ -1,10 +1,22 @@
 module Heracles
   module Admin
-
     class BulkPublicationController < Heracles::Admin::ApplicationController
-      def index
-      end
-    end
+      helper SearchesHelper
 
+      def index
+        if params[:q]
+          tags         = params[:q].split(",")
+          search_scope = params[:scope] == "published" ? true : false
+
+          @search = Sunspot.search(Heracles::Site.first.page_classes) do
+            with :tags, tags
+            with :published, search_scope
+            order_by :created_at, :desc
+            paginate page: params[:page] || 1, per_page: 40
+          end
+        end
+      end
+
+    end
   end
 end
