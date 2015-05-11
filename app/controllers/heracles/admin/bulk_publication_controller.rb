767f6e0d08df5ec53615a881ba5915f95c457079
query-string <atimofeev@reactant.ru>
2015-05-11 11:08:25 +0700

    Add publish action.

--- app/controllers/heracles/admin/bulk_publication_controller.rb
+++ app/controllers/heracles/admin/bulk_publication_controller.rb
@@ -5,23 +5,29 @@ module Heracles
 
       def index
         if params[:q]
-          tags = params[:q].split(",")
-
-          @search = Sunspot.search(Heracles::Site.first.page_classes) do
-            with :tags, tags
-            with :published, true
-            with :site_id, current_site.id
-            order_by :created_at, :desc
-            facet :page_type
-            paginate page: params[:page] || 1, per_page: 40
-          end
-
+          @search = query params, 40
           @facets = @search.facet(:page_type).rows
         end
       end
 
       def create
-        p params
+        search = query params, 100
+        search.results.size
+      end
+
+    private
+
+      def query(params, per_page)
+        tags = params[:q].split(",")
+
+        Sunspot.search(Heracles::Site.first.page_classes) do
+          with :tags, tags
+          with :published, true
+          with :site_id, current_site.id
+          order_by :created_at, :desc
+          facet :page_type
+          paginate page: params[:page] || 1, per_page: per_page
+        end
       end
 
     end
