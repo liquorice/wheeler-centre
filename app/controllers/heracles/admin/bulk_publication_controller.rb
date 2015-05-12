29d423f4b8ac606c3e3cd6ac63cf4e9c32636446
query-string <atimofeev@reactant.ru>
2015-05-12 17:10:52 +0700

    Extract publish/unpublish action to background job.

--- app/controllers/heracles/admin/bulk_publication_controller.rb
+++ app/controllers/heracles/admin/bulk_publication_controller.rb
@@ -1,12 +1,13 @@
 module Heracles
   module Admin
     class BulkPublicationController < Heracles::Admin::ApplicationController
-      helper SearchesHelper
+      include BulkPublicationSearch
       include ActionView::Helpers::TextHelper
+      helper SearchesHelper
 
       def index
         if query_defined?
-          @search = sunpot_query params, 40
+          @search = sunpot_query params, current_site.id, 40
           @facets = @search.facet(:page_type).rows
         end
       end
@@ -26,16 +27,6 @@ module Heracles
             BulkPublicationJob.enqueue action.id
             redirect_to :back, flash: {success: "Status of selected records will be changed to #{status}ed very soon..."}
           end
-=begin
-          # Get all available records count (otherwise Sunspot returns only 40 per page)
-          total_records = Heracles::Site.first.page_classes.map{ |item| item.all.count }.inject(:+)
-          # Feed total records size to Sunspot
-          total_search = sunpot_query(params, total_records).results
-          # Change published status
-          total_search.each{ |record| record.update(published: params.has_key?(:publish) ? true : false) } if total_search.size > 0
-          # Redirect
-          redirect_to :back, flash: {success: "Status changed for #{pluralize(total_search.size, "record")}"}
-=end
         else
           redirect_to :back, flash: {alert: "Query parameter must be defined"}
         end
@@ -45,20 +36,6 @@ module Heracles
 
       def query_defined?
         true if params[:q] && params[:q].present?
-      end
-
-      def sunpot_query(params, per_page)
-        tags = params[:q].split(",")
-
-        Sunspot.search(Heracles::Site.first.page_classes) do
-          all_of do
-            tags.each{ |tag| with :tags, tag }
-          end
-          with :site_id, current_site.id
-          order_by :created_at, :desc
-          facet :page_type
-          paginate page: params[:page] || 1, per_page: per_page
-        end
       end
 
     end
