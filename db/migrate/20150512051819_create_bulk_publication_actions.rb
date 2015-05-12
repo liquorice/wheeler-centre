06804723983f4b1c9fbbd865da8aa0f364bf0f3a
query-string <atimofeev@reactant.ru>
2015-05-12 12:39:26 +0700

    Create AR model for tracking of bulk publish jobs.

---
+++ db/migrate/20150512051819_create_bulk_publication_actions.rb
@@ -0,0 +1,12 @@
+class CreateBulkPublicationActions < ActiveRecord::Migration
+  def change
+    create_table :bulk_publication_actions do |t|
+      t.integer :user_id
+      t.text :tags
+      t.string :action
+
+      t.datetime :created_at
+      t.datetime :completed_at
+    end
+  end
+end
