b821f9f4a3daf09f9dc1323db7b67581d20a99df
query-string <atimofeev@reactant.ru>
2015-05-13 15:45:59 +0700

    Show unpublished actions box on the bulk publish page.

--- app/models/bulk_publication_action.rb
+++ app/models/bulk_publication_action.rb
@@ -1,3 +1,9 @@
 class BulkPublicationAction < ActiveRecord::Base
   validates :site_id, :user_id, :tags, :action, presence: true
+
+  scope :inprogress, ->(site_id, user_id) { where("site_id = ? AND user_id = ? AND completed_at IS ?", site_id, user_id, nil).order("id DESC") }
+
+  def readable_tags
+    tags.split(",").join(", ")
+  end
 end
