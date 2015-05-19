79ce631813b17b959eeb5009d3b64c4562deae82
Max Wheeler <max@makenosound.com>
2015-05-19 13:40:46 +1000

    Add pretty rough templates for Texts in the City.

--- app/models/heracles/sites/wheeler_centre/texts_in_the_city.rb
+++ app/models/heracles/sites/wheeler_centre/texts_in_the_city.rb
@@ -5,9 +5,14 @@ module Heracles
         def self.config
           {
             fields: [
+              {name: :intro, type: :content},
               {name: :body, type: :content}
             ]
           }
+        end
+
+        def books
+          children.of_type("texts_in_the_city_book").visible.published
         end
       end
     end
