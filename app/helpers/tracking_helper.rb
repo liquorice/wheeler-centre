44e1ba5e204aa2d093aed78ed922351d7248b7b9
Dylan Wolff <dylanwolff@gmail.com>
2015-10-19 09:41:18 +1100

    Be more explicit in naming of helper methods & allow passing in of location, title and path in options hash

--- app/helpers/tracking_helper.rb
+++ app/helpers/tracking_helper.rb
@@ -3,13 +3,13 @@ module TrackingHelper
     localhost
   ).freeze
 
-  def event_url(target, options = {})
+  def track_event_url(target, options = {})
     if PERMITTED_HOSTS.any? { |h| target.include? h }
       track_event_path(
-        location: url_with_domain(page.absolute_url),
-        title: page.title,
-        path: page.absolute_url,
         target: target,
+        location: options[:location],
+        title: options[:title],
+        path: options[:path],
         category: options[:category],
         track_action: options[:track_action],
         label: target)
@@ -18,26 +18,26 @@ module TrackingHelper
     end
   end
 
-  def pageview_url(target)
+  def track_pageview_url(target)
     if PERMITTED_HOSTS.any? { |h| target.include? h }
       url = track_pageview_path(
-        location: url_with_domain(page.absolute_url),
-        title: page.title,
-        path: page.absolute_url,
+        location: options[:location],
+        title: options[:title],
+        path: options[:path],
         campaign_id: "png")
-      raw("<img src=#{url}, width=1, height=1>")
+      raw("<img src=#{url} width=1 height=1>")
     else
       target
     end
   end
 
-  def social_url(target, options = {})
+  def track_social_url(target, options = {})
     if PERMITTED_HOSTS.any? { |h| target.include? h }
       track_social_path(
-        location: url_with_domain(page.absolute_url),
-        title: page.title,
-        path: page.absolute_url,
         target: target,
+        location: options[:location],
+        title: options[:title],
+        path: options[:path],
         track_action: options[:action],
         network: options[:network])
     else
