78637c7eec3115ca174453b0a8f1717b956caef0
Narinda Reeders <narindajane@gmail.com>
2016-02-04 18:12:39 +1100

    New model for adopt a word archive. Searches parent page for words that are not available

---
+++ app/models/heracles/sites/wheeler_centre/adopt_a_word_archive.rb
@@ -0,0 +1,43 @@
+module Heracles
+  module Sites
+    module WheelerCentre
+      class AdoptAWordArchive < ::Heracles::Page
+        include ApplicationHelper
+
+        def self.config
+          {
+            fields: [
+              {name: :intro, type: :content},
+            ]
+          }
+        end
+
+        def words(options={})
+          search_words(options)
+        end
+
+        def words_index
+          parent
+        end
+
+        private
+
+        def words_index
+          parent
+        end
+
+        def search_words(options={})
+          Sunspot.search(CampaignWord) do
+            with :site_id, site.id
+            with :parent_id, words_index.id
+            with :published, true
+            with :hidden, false
+            with :available, false
+
+            order_by :title, :asc
+          end
+        end
+      end
+    end
+  end
+end
