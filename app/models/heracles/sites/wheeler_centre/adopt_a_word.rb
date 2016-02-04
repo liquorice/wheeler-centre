d88f220210964645a81ff92c4e0f09e6bf59ef6b
Narinda Reeders <narindajane@gmail.com>
2016-02-04 18:11:04 +1100

    Add associated articles to adopt a word page

--- app/models/heracles/sites/wheeler_centre/adopt_a_word.rb
+++ app/models/heracles/sites/wheeler_centre/adopt_a_word.rb
@@ -11,16 +11,32 @@ module Heracles
               {name: :description_right, type: :content, disable_insertables: true},
               {name: :list_description, type: :text },
               {name: :donation_intro, type: :content, disable_insertables: true},
-              {name: :donation_info, type: :content, disable_insertables: true}
+              {name: :donation_info, type: :content, disable_insertables: true},
+              {name: :articles, type: :associated_pages, page_type: :blog_post}
             ]
           }
         end
 
         ### Accessors
-        def words
-          children.of_type("campaign_word").published.order(:title)
+        def words(options={})
+          search_words(options)
+
+          #children.of_type("campaign_word").published.order(:title)
         end
 
+        private
+
+        def search_words(options={})
+          Sunspot.search(CampaignWord) do
+            with :site_id, site.id
+            with :parent_id, id
+            with :published, true
+            with :hidden, false
+            with :available, true
+
+            order_by :title, :asc
+          end
+        end
       end
     end
   end
