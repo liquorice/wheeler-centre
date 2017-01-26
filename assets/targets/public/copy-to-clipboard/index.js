f34a084f5f29fc61d44dbb3246f749c5bb9a963d
Andy McCray <andy@icelab.com.au>
2017-01-26 16:57:28 +1100

    Show a confirmation tooltip when copying to the clipboard.

--- assets/targets/public/copy-to-clipboard/index.js
+++ assets/targets/public/copy-to-clipboard/index.js
@@ -1,16 +1,46 @@
 var Clipboard = require("clipboard");
 
-function CopyToClipboard(el, props) {
+/**
+ * Copy the contents of an element to the clipboard
+ * @param  {Node}   el    The element we want to hold our count
+ * @param  {Object} props Various options:
+ *                        - {String} triggerEl
+ *                        - {String} targetEl
+                          - {Bool}   showTooltip
+ * @return {Void}
+ */
+function copyToClipboard(el, props) {
   var triggerEl = el.querySelector(props.triggerElement);
   var targetEl = el.querySelector(props.targetElement);
+  var showTooltip = props.showTooltip;
   var clipboard = new Clipboard(triggerEl, {
     target: function() {
       return targetEl;
     }
   });
-  clipboard.on('success', function(e) {
+  clipboard.on("success", function(e) {
     e.clearSelection();
+    // Should we show a confirmation tooltip?
+    if (showTooltip) {
+      createTooltip(triggerEl, "Copied");
+    }
   });
 }
 
-module.exports = CopyToClipboard;
+/**
+ * Create a tooltip
+ * @param  {Node}   targetEl The element to append the tooltip
+ * @param  {String} text     Contents of the tooltip
+ * @return {Void}
+ */
+function createTooltip(targetEl, text) {
+  var tooltipEl = document.createElement("span");
+  tooltipEl.className = "copy-tooltip";
+  tooltipEl.innerHTML = text;
+  targetEl.appendChild(tooltipEl);
+  setTimeout(function(){
+    targetEl.removeChild(tooltipEl);
+  },1000);
+}
+
+module.exports = copyToClipboard;
