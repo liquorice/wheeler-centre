16b071dbb23f5dfcd8692a7261cada112f00b3f5
Toby Allder <toby@icelab.com.au>
2017-03-08 13:42:11 +1100

    Updating the set of sponsor groups on the Our Partners page

--- app/models/heracles/sites/wheeler_centre/sponsors.rb
+++ app/models/heracles/sites/wheeler_centre/sponsors.rb
@@ -7,12 +7,22 @@ module Heracles
             fields: [
               {name: :intro, label: "Introduction", type: :content},
               {name: :body, type: :content},
+
               {name: :government_partners, type: :associated_pages, page_type: :sponsor},
               {name: :patrons_content, type: :content},
-              {name: :major_sponsors, type: :associated_pages, page_type: :sponsor},
-              {name: :trusts_foundations, type: :associated_pages, page_type: :sponsor},
-              {name: :ministry_of_ideas, type: :associated_pages, page_type: :sponsor},
+              {name: :major_partners, type: :associated_pages, page_type: :sponsor},
+
+              {name: :accomodation, type: :associated_pages, page_type: :sponsor},
+              {name: :audio_visual, type: :associated_pages, page_type: :sponsor},
+              {name: :fellowships_and_residencies, type: :associated_pages, page_type: :sponsor},
+              {name: :special_projects, type: :associated_pages, page_type: :sponsor},
+
+              #{name: :trusts_foundations, type: :associated_pages, page_type: :sponsor},
+              #{name: :ministry_of_ideas, type: :associated_pages, page_type: :sponsor},
+
               {name: :venue_partners, type: :associated_pages, page_type: :sponsor},
+              {name: :resident_organisations, type: :associated_pages, page_type: :sponsor},
+
             ]
           }
         end
