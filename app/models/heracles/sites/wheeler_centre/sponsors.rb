7058b82360038ac1bebce921aae21a9da860af2c
Toby Allder <toby@icelab.com.au>
2017-03-11 16:40:34 +1100

    Adding missing sponsor groups and fixing typo in admin

--- app/models/heracles/sites/wheeler_centre/sponsors.rb
+++ app/models/heracles/sites/wheeler_centre/sponsors.rb
@@ -10,13 +10,17 @@ module Heracles
               {name: :government_partners, type: :associated_pages, page_type: :sponsor},
               {name: :patrons_content, type: :content},
               {name: :major_partners, type: :associated_pages, page_type: :sponsor},
-              {name: :accomodation, type: :associated_pages, page_type: :sponsor},
+              {name: :media, type: :associated_pages, page_type: :sponsor},
+              {name: :corporate_partners, type: :associated_pages, page_type: :sponsor},
+              {name: :accommodation, type: :associated_pages, page_type: :sponsor},
               {name: :audio_visual, type: :associated_pages, page_type: :sponsor},
+              {name: :wine, type: :associated_pages, page_type: :sponsor},
               {name: :fellowships_and_residencies, type: :associated_pages, page_type: :sponsor},
               {name: :special_projects, type: :associated_pages, page_type: :sponsor},
+              {name: :reimagining_the_performace_space, type: :associated_pages, page_type: :sponsor},
+              {name: :digital_engagement, type: :associated_pages, page_type: :sponsor},
               {name: :venue_partners, type: :associated_pages, page_type: :sponsor},
               {name: :resident_organisations, type: :associated_pages, page_type: :sponsor},
-
             ]
           }
         end
@@ -24,3 +28,7 @@ module Heracles
     end
   end
 end
+
+
+
+
