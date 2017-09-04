1cbac58d0bd3fa85a538eaf57df0af2fe1ebc201
narinda <narindajane@gmail.com>
2017-09-04 16:44:35 +1000

    Some settings additions

--- app/models/heracles/sites/wheeler_centre/ticker_text.rb
+++ app/models/heracles/sites/wheeler_centre/ticker_text.rb
@@ -2,6 +2,14 @@ module Heracles
   module Sites
     module WheelerCentre
       class TickerText < ::Heracles::Page
+        ### Summary
+
+        def to_summary_hash
+          {
+            title: title,
+            published: (published) ? "✔" : "•"
+          }
+        end
       end
     end
   end
