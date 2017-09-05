817a76df1f36098e165a92823cd7ff2c71dec284
narinda <narindajane@gmail.com>
2017-09-05 10:53:39 +1000

    Attempt a resize function to calculate the scroller widths and duration

--- assets/targets/public/footer-ticker/index.js
+++ assets/targets/public/footer-ticker/index.js
@@ -5,7 +5,6 @@ function FooterTicker(el, props) {
   this.el = el;
   this.innerEl = el.querySelector(".ticker__inner");
   this.cookiePrefix = (props.cookiePrefix) ? props.cookiePrefix+"-footer-ticker" : "footer-ticker";
-  console.log(this.cookiePrefix);
   var close = el.querySelector(".ticker__close");
   var isRegistered = tourist.isRegistered(this.cookiePrefix);
   if (!isRegistered) {
@@ -17,6 +16,9 @@ function FooterTicker(el, props) {
     close.parentNode.removeChild(close);
   }
   close.addEventListener("click", this.onCloseClick.bind(this));
+
+  window.addEventListener("resize", this.onResize.bind(this));
+
 }
 
 FooterTicker.prototype.onCloseClick = function(e) {
@@ -24,6 +26,16 @@ FooterTicker.prototype.onCloseClick = function(e) {
   tourist.register(this.cookiePrefix, true);
   addClass(this.el, ["ticker--hide"]);
   this.innerEl.style.marginTop = this.el.offsetHeight + "px";
+};
+
+FooterTicker.prototype.onResize = function() {
+  var scroller = this.el.querySelector(".ticker__scroller");
+  var scrollerWrapper = this.el.querySelector(".ticker__scroller-wrapper");
+  console.log(scrollerWrapper.offSetWidth);
+  var duration = parseInt((1/scroller.offSetWidth) * 1000) + "s";
+  console.log(duration);
+  scroller.setAttribute("padding-right", scrollerWrapper.offSetWidth + "px");
+  scroller.setAttribute("animation-duration", duration);
 };
 
 module.exports = FooterTicker;
