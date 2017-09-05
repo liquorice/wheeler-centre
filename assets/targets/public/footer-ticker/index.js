eb8ff5944923b330a7e605a3d677a13b579fbc70
narinda <narindajane@gmail.com>
2017-09-05 14:22:02 +1000

    Adjust the animation duration according to the size of the scrolling text

--- assets/targets/public/footer-ticker/index.js
+++ assets/targets/public/footer-ticker/index.js
@@ -17,8 +17,9 @@ function FooterTicker(el, props) {
   }
   close.addEventListener("click", this.onCloseClick.bind(this));
 
-  window.addEventListener("resize", this.onResize.bind(this));
-
+  var scroller = this.el.querySelector(".ticker__scroller");
+  var duration = Math.floor(scroller.offsetWidth/60) + "s";
+  scroller.style["animation-duration"] = duration;
 }
 
 FooterTicker.prototype.onCloseClick = function(e) {
@@ -26,16 +27,6 @@ FooterTicker.prototype.onCloseClick = function(e) {
   tourist.register(this.cookiePrefix, true);
   addClass(this.el, ["ticker--hide"]);
   this.innerEl.style.marginTop = this.el.offsetHeight + "px";
-};
-
-FooterTicker.prototype.onResize = function() {
-  var scroller = this.el.querySelector(".ticker__scroller");
-  var scrollerWrapper = this.el.querySelector(".ticker__scroller-wrapper");
-  console.log(scrollerWrapper.offSetWidth);
-  var duration = parseInt((1/scroller.offSetWidth) * 1000) + "s";
-  console.log(duration);
-  scroller.setAttribute("padding-right", scrollerWrapper.offSetWidth + "px");
-  scroller.setAttribute("animation-duration", duration);
 };
 
 module.exports = FooterTicker;
