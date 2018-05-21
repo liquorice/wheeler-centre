eeb2096d64b4b1f901a6f5276354abd7ddeb4839
Toby Allder <toby@icelab.com.au>
2018-05-21 12:17:54 +1000

    Changing sponsor page admin fields

--- app/models/heracles/sites/wheeler_centre/sponsors.rb
+++ app/models/heracles/sites/wheeler_centre/sponsors.rb
@@ -7,17 +7,12 @@ module Heracles
             fields: [
               {name: :intro, label: "Introduction", type: :content},
               {name: :body, type: :content},
-              {name: :government_partners, type: :associated_pages, page_type: :sponsor},
               {name: :patrons_content, type: :content},
-              {name: :media, type: :associated_pages, page_type: :sponsor},
-              {name: :corporate_partners, type: :associated_pages, page_type: :sponsor},
-              {name: :accommodation, type: :associated_pages, page_type: :sponsor},
-              {name: :audio_visual, type: :associated_pages, page_type: :sponsor},
-              {name: :wine, type: :associated_pages, page_type: :sponsor},
-              {name: :fellowships_and_residencies, type: :associated_pages, page_type: :sponsor},
-              {name: :reimagining_the_performace_space, type: :associated_pages, page_type: :sponsor},
-              {name: :digital_engagement, type: :associated_pages, page_type: :sponsor},
-              {name: :venue_partners, type: :associated_pages, page_type: :sponsor},
+              {name: :government_partners, type: :associated_pages, page_type: :sponsor},
+              {name: :major_partners, type: :associated_pages, page_type: :sponsor},
+              {name: :program_partners, type: :associated_pages, page_type: :sponsor},
+              {name: :supporting_partners, type: :associated_pages, page_type: :sponsor},
+              {name: :trusts_and_foundations, type: :associated_pages, page_type: :sponsor},
               {name: :resident_organisations, type: :associated_pages, page_type: :sponsor},
             ]
           }
