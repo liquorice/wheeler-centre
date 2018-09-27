c34940cb95855235abd9c4efbf85d1241cff195b
Max Wheeler <max@makenosound.com>
2018-09-27 10:09:39 +1000

    Simplify gallery view code and use thumbnails

--- app/insertables/heracles/sites/wheeler_centre/gallery_insertable_renderer.rb
+++ app/insertables/heracles/sites/wheeler_centre/gallery_insertable_renderer.rb
@@ -29,8 +29,8 @@ module Heracles
         helper_method \
         def gallery_slide_data
           assets.each_with_index.map {|asset, i|
-            version_name  = :content_small
-            version_name  = :original unless asset.versions.include?(:content_small)
+            version_name = :content_large
+            version_name = :original unless asset.versions.include?(:content_large)
             {
               image: asset.send(:"#{version_name.to_sym}_url"),
               caption: data[:assets_data][i][:caption]
@@ -38,6 +38,21 @@ module Heracles
           }
         end
 
+        helper_method \
+        def images
+          assets.each_with_index.map {|asset, i|
+            asset_data = data[:assets_data][i]
+            thumbnail_version_name = :content_thumbnail
+            thumbnail_version_name = :original unless asset.versions.include?(:content_thumbnail)
+            large_version_name = :content_large
+            large_version_name = :original unless asset.versions.include?(:content_large)
+            {
+              thumbnail_url: asset.send(:"#{thumbnail_version_name.to_sym}_url"),
+              alt_text: asset_data[:alt_text],
+              link_url: asset_data[:link].present? ? asset_data[:link] : asset.send(:"#{large_version_name.to_sym}_url")
+            }
+          }
+        end
 
         private
 
