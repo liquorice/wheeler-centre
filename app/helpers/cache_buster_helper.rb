e832ca924b6b1de0c3b7bf24be4528c3eea48657
Nick Binnington <nick.binnington@gmail.com>
2018-10-31 10:56:54 +1100

    Fix up the cache buster script helper to handle certain nextchapter urls

--- app/helpers/cache_buster_helper.rb
+++ app/helpers/cache_buster_helper.rb
@@ -5,7 +5,10 @@ module CacheBusterHelper
 
     host = (request.port == 80 ? request.host : request.host_with_port)
 
-    request_path = request.path.gsub(/^\/thenextchapter/, "")
+    request_path = request.path
+    if request.host =~ /^thenextchapter/
+      request_path.gsub!(/^\/thenextchapter/, "")
+    end
 
     src = "//#{site.primary_origin_hostname}/_check/#{host}#{request_path}/_check.js#{('?' + request.query_string) if request.query_string.present?}"
     content_tag :script, nil, async: true, src: src
