dc42853ed70eac2ad65171202d5df6148f105315
Max Wheeler <max@makenosound.com>
2019-01-14 16:01:51 +1100

    Add sponsors and topics to event export

--- app/models/heracles/sites/wheeler_centre/sponsor.rb
+++ app/models/heracles/sites/wheeler_centre/sponsor.rb
@@ -12,6 +12,34 @@ module Heracles
             ]
           }
         end
+
+        def csv_headers
+          [
+            "ID",
+            "Title",
+            "URL",
+            "Body",
+            "Sponsor URL",
+            "Logo",
+            "Sponsor ID",
+            "Updated at",
+            "Created at",
+          ]
+        end
+
+        def to_csv
+          [
+            id,
+            title,
+            absolute_url,
+            fields[:body],
+            fields[:url],
+            (fields[:logo].assets.first.original_url if fields[:logo].data_present?),
+            fields[:sponsor_id],
+            updated_at,
+            created_at,
+          ]
+        end
       end
     end
   end
