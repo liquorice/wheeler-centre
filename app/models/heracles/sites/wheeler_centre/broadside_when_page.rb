64fc1c86197ce744ac1564e3e3f11be08c0abfa8
Nick Binnington <nick.binnington@gmail.com>
2019-09-12 22:03:44 +1000

    Only show published & visible event_pages on /when

--- app/models/heracles/sites/wheeler_centre/broadside_when_page.rb
+++ app/models/heracles/sites/wheeler_centre/broadside_when_page.rb
@@ -31,8 +31,8 @@ module Heracles
         def event_pages
           # This is done this way so we get the event_pages in date order of the associated event... N+1; probably a better way of doing this.
           broadside_events.map do |event|
-            site.pages.of_type(:broadside_event_page).where("(fields_data#>'{event, page_ids}')::jsonb ?| ARRAY[:page_ids]", page_ids: [event.id]).first
-          end
+            site.pages.of_type(:broadside_event_page).where("(fields_data#>'{event, page_ids}')::jsonb ?| ARRAY[:page_ids]", page_ids: [event.id]).published.visible.first
+          end.compact
         end
 
         def event_pages_grouped_by_day
