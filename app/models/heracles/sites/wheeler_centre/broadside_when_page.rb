dd6cffb5bef67d5a52420547ff40a71ef7ce0eda
Nick Binnington <nick.binnington@gmail.com>
2019-09-12 21:57:36 +1000

    Add fragment caching to the Broadside when page

--- app/models/heracles/sites/wheeler_centre/broadside_when_page.rb
+++ app/models/heracles/sites/wheeler_centre/broadside_when_page.rb
@@ -21,14 +21,16 @@ module Heracles
           super.gsub(/^\/broadside/, "")
         end
 
+        def broadside_events
+          broadside_series = Heracles::Page.of_type(:event_series).find_by(title: "Broadside")
+          Heracles::Page.of_type(:event).where("(fields_data#>'{series, page_ids}')::jsonb ?| ARRAY[:page_ids]", page_ids: broadside_series.id).order("fields_data->'start_date'->>'value' ASC NULLS LAST")
+        end
+
         private
 
         def event_pages
           # This is done this way so we get the event_pages in date order of the associated event... N+1; probably a better way of doing this.
-          broadside_series = Heracles::Page.of_type(:event_series).find_by(title: "Broadside")
-          events = Heracles::Page.of_type(:event).where("(fields_data#>'{series, page_ids}')::jsonb ?| ARRAY[:page_ids]", page_ids: broadside_series.id).
-                    order("fields_data->'start_date'->>'value' ASC NULLS LAST")
-          events.map do |event|
+          broadside_events.map do |event|
             site.pages.of_type(:broadside_event_page).where("(fields_data#>'{event, page_ids}')::jsonb ?| ARRAY[:page_ids]", page_ids: [event.id]).first
           end
         end
