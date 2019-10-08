e2962772ebb5a40b7346cfcbe0df35c7318c7a55
Tim Riley <tim@openmonkey.com>
2019-10-09 10:29:56 +1100

    Add special-casing back for enqueuing thenextchapter page cache checks
    It seems to be needed

--- app/helpers/cache_buster_helper.rb
+++ app/helpers/cache_buster_helper.rb
@@ -5,7 +5,14 @@ module CacheBusterHelper
 
     host = (request.port == 80 ? request.host : request.host_with_port)
 
-    src = "//#{site.primary_origin_hostname}/_check/#{host}#{request.path}/_check.js#{('?' + request.query_string) if request.query_string.present?}"
+    request_path =
+      if request.host =~ /^thenextchapter/
+        request.path.gsub(/^\/thenextchapter/, "")
+      else
+        request.path
+      end
+
+    src = "//#{site.primary_origin_hostname}/_check/#{host}#{request_path}/_check.js#{('?' + request.query_string) if request.query_string.present?}"
     content_tag :script, nil, async: true, src: src
   end
 end
