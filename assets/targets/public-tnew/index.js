0b1e614df7a2e2357e9f7da3327fa13a12f12205
Max Wheeler <max@makenosound.com>
2019-10-15 16:46:24 +1100

    Create separate JS target for TNEW

---
+++ assets/targets/public-tnew/index.js
@@ -0,0 +1,42 @@
+require("es5-shim");
+var viewloader = require("viewloader");
+
+// Construct only TNEW template relevant views
+var views = {};
+
+var FastClick = require("fastclick");
+views.fastClick = function(el, props) {
+  new FastClick(el);
+};
+
+var MastheadEvents = require("../public/masthead-events");
+views.mastheadEvents = function(el, props) {
+  new MastheadEvents(el, props);
+};
+
+var MastheadSearch = require("../public/masthead-search");
+views.mastheadSearch = function(el, props) {
+  new MastheadSearch(el, props);
+};
+
+var NavToggle = require("../public/nav-toggle");
+views.navToggle = function(el, props) {
+  new NavToggle(el, props);
+};
+
+// Search
+var Search = require("../public/search");
+views.search = function(el) {
+  new Search(el);
+};
+
+var TouchPreview = require("../public/touch-preview");
+views.touchPreview = function(el, previewClassName) {
+  new TouchPreview(el, previewClassName);
+};
+
+
+// Kick things off
+domready(function onDomReady() {
+  viewloader.execute(views);
+});
