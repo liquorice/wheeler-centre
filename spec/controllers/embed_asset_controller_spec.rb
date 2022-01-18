3840aa1c4c3de275591dcb5c694aeaf394ef7dd2
Tsengel Purevdorj <tsengel.purevdorj@reinteractive.com>
2022-01-19 10:49:34 +1100

    Create spec for models, controllers, helpers and mailers

---
+++ spec/controllers/embed_asset_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe EmbedAssetController, type: :controller do
+
+end
---
+++ spec/mailers/previews/content_export_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/content_export_mailer
+class ContentExportMailerPreview < ActionMailer::Preview
+
+end
