daf328f2a5454f91c41a13fb18512d4c3817d9d2
Tsengel Purevdorj <tsengel.purevdorj@reinteractive.com>
2022-01-19 13:00:03 +1100

    Add spec for BulkPublicationAction

--- spec/models/bulk_publication_action_spec.rb
+++ spec/models/bulk_publication_action_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+require 'securerandom'
 
 RSpec.describe BulkPublicationAction, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do   
+    it 'passes for :site_id' do
+      expect validate_presence_of(:site_id)
+    end
+    it { expect validate_presence_of(:user_id) }
+    it { expect validate_presence_of(:tags) }
+    it { expect validate_presence_of(:action) }
+  end
+
+  describe 'Scope' do
+    it 'in_progress works' do
+      expect(described_class.in_progress(SecureRandom.uuid,1)).to eq([])
+    end
+  end
 end
